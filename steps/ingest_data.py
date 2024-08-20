4543909b036a77b13edcc1b0336502f3b72c7b5a
Brice PIAPLE DADA <67214291+bpiaple@users.noreply.github.com>
2024-08-20 21:16:03 +0000

    first

--- steps/ingest_data.py
+++ steps/ingest_data.py
@@ -4,6 +4,9 @@ import pandas as pd
 from zenml import step
 
 class IngestData:
+    """
+    Ingest
+    """
     def __init__(self, data_path: str):
         self.data_path = data_path
 
@@ -14,5 +17,14 @@ class IngestData:
 
 @step
 def ingest_data(data_path: str) -> pd.DataFrame:
+    """
+    Ingesting the data from data_path
+
+    Args:
+        data_path: path to the data
+
+    Returns:
+        pd.DataFrame: the ingested data
+    """
     try:
-        
+
