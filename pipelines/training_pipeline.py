3f6c4777880cde48805bf9ae52d672386fee7813
Brice PIAPLE DADA <67214291+bpiaple@users.noreply.github.com>
2024-08-21 04:00:48 +0000

    chore: disable cache in training pipeline

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -6,7 +6,7 @@ from steps.evaluation import evaluate_model
 from steps.config import ModelNameConfig
 
 
-@pipeline(enable_cache=True)
+@pipeline(enable_cache=False)
 def training_pipeline(data_path: str):
     df = ingest_data(data_path)
     X_train, X_test, y_train, y_test = clean_data(df)
